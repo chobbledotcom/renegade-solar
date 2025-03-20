d5c342b59e9ebd6a2ff6cfdd849c0951310d40c4
Stefan <git@stefn.co.uk>
2025-03-20 22:18:35 +0000

    Add sizes attribute

--- .eleventy.js
+++ .eleventy.js
@@ -3,55 +3,56 @@ const path = require("path");
 const { Image, eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 
 module.exports = async function (eleventyConfig) {
-	const { EleventyRenderPlugin } = await import("@11ty/eleventy");
+  const { EleventyRenderPlugin } = await import("@11ty/eleventy");
 
-	eleventyConfig.addPlugin(EleventyRenderPlugin);
-	eleventyConfig.addWatchTarget("./src/**/*");
-	eleventyConfig.addPassthroughCopy("src/assets");
-	eleventyConfig.addPassthroughCopy({
-		"src/assets/favicon.ico": "/favicon.ico",
-	});
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
+  eleventyConfig.addWatchTarget("./src/**/*");
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy({
+    "src/assets/favicon.ico": "/favicon.ico",
+  });
 
-	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-		formats: ["auto"],
-		widths: ["auto"],
-		svgShortCircuit: "size",
-		htmlOptions: {
-			imgAttributes: {
-				loading: "lazy",
-				decoding: "async",
-			},
-		},
-	});
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    formats: ["auto"],
+    widths: ["auto"],
+    svgShortCircuit: "size",
+    htmlOptions: {
+      imgAttributes: {
+        sizes: "auto",
+        loading: "lazy",
+        decoding: "async",
+      },
+    },
+  });
 
-	eleventyConfig.addShortcode("image", async (src, alt, sizes) => {
-		let metadata = await Image(src, {
-			widths: [150, 300],
-			formats: ["webp", "jpeg"],
-			outputDir: "./_site/img/",
-		});
+  eleventyConfig.addShortcode("image", async (src, alt, sizes) => {
+    let metadata = await Image(src, {
+      widths: [150, 300],
+      formats: ["webp", "jpeg"],
+      outputDir: "./_site/img/",
+    });
 
-		let imageAttributes = {
-			alt,
-			sizes,
-			loading: "lazy",
-			decoding: "async",
-		};
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: "lazy",
+      decoding: "async",
+    };
 
-		// You bet we throw an error on a missing alt (alt="" works okay)
-		return Image.generateHTML(metadata, imageAttributes);
-	});
+    // You bet we throw an error on a missing alt (alt="" works okay)
+    return Image.generateHTML(metadata, imageAttributes);
+  });
 
-	return {
-		dir: {
-			input: "src",
-			output: "_site",
-			includes: "_includes",
-			layouts: "_layouts",
-			data: "_data",
-		},
-		templateFormats: ["liquid", "njk", "md"],
-		htmlTemplateEngine: "liquid",
-		markdownTemplateEngine: "liquid",
-	};
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "_data",
+    },
+    templateFormats: ["liquid", "njk", "md"],
+    htmlTemplateEngine: "liquid",
+    markdownTemplateEngine: "liquid",
+  };
 };
