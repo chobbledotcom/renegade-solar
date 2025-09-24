81f6fc838f7b3e184173a38f1f83efbff211a1d1
Stefan <git@stefn.co.uk>
2025-09-24 17:54:52 +0100

    Rejig to use Github actions / npm

--- flake.nix
+++ flake.nix
@@ -1,85 +1,62 @@
 {
-  description = "renegade-solar.co.uk";
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs";
   };
+
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        nodeDeps = import ./node-deps.nix { inherit pkgs; };
-        inherit (nodeDeps) packageJSON nodeModules;
+    { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
 
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+      makeEnvForSystem =
+        system:
+        let
+          pkgs = import nixpkgs { system = system; };
 
-        # Common build inputs
-        commonBuildInputs = with pkgs; [
-          djlint
-          sass
-          vscode-langservers-extracted
-          yarn
-          yarn2nix
-        ];
-
-        # Helper function to create scripts
-        mkScript =
-          name:
-          (pkgs.writeScriptBin name (builtins.readFile ./bin/${name})).overrideAttrs (old: {
-            buildCommand = "${old.buildCommand}\n patchShebangs $out";
-          });
-
-        # Helper function to create packages
-        mkPackage =
-          name:
-          pkgs.symlinkJoin {
-            inherit name;
-            paths = [ (mkScript name) ] ++ commonBuildInputs;
-            buildInputs = [ pkgs.makeWrapper ];
-            postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+          devDependencies = with pkgs; [
+            nodejs_23
+            biome
+          ];
+        in
+        {
+          inherit pkgs;
+          devEnv = {
+            inherit pkgs;
+            dependencies = devDependencies;
           };
+        };
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          env = makeEnvForSystem system;
+          inherit (env.devEnv)
+            pkgs
+            dependencies
+            ;
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = dependencies;
 
-        # Script names
-        scripts = [
-          "build"
-          "serve"
-        ];
-
-        # Generate all packages
-        scriptPackages = builtins.listToAttrs (
-          map (name: {
-            inherit name;
-            value = mkPackage name;
-          }) scripts
-        );
-
-      in
-      rec {
-        defaultPackage = packages.serve;
-        packages = scriptPackages;
-
-        devShells = rec {
-          default = dev;
-          dev = pkgs.mkShell {
-            buildInputs = commonBuildInputs ++ (builtins.attrValues packages);
             shellHook = ''
-              rm -rf node_modules
-              rm -rf package.json
-              ln -sf ${packageJSON} package.json
-              ln -sf ${nodeModules}/node_modules .
-              echo "Development environment ready!"
-              echo "Run 'serve' to start development server"
-              echo "Run 'build' to build the site in the _site directory"
+              cat <<EOF
+
+              Development environment ready!
+
+              Available commands:
+               - 'npm run serve'   # Start development server
+               - 'npm run build'   # Build the site in the _site directory
+               - 'lint'            # Lint all files using Biome
+
+              EOF
+
               git pull
             '';
           };
-        };
-      }
-    );
+        }
+      );
+    };
 }
